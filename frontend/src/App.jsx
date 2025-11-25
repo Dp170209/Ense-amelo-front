fba4d13c5d44f1f4784b6f73469e466e34f10dca
unknown <fmattosteran9000@gmail.com>
2025-11-25 19:16:23 -0400

    carpeta frontend react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,56 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
+import Explorar from "./components/Explorar/Explorar";
+import MisCursos from "./components/MisCursos/MisCursos";
+import InfoCurso from "./components/InfoCurso/InfoCurso";
+import Navbar from "./components/Navbar";
+
+// PANEL DE PAGOS
+import PaymentsDashboard from "./components/Payments-Service/PaymentDashboard";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-100">
+      <Routes>
+
+        {/* Login y registro */}
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/payments-service-panel" element={<PaymentsDashboard />} />
+
+        {/* Explorar */}
+        <Route path="/explorar" element={
+          <>
+            <Navbar currentSection="explore" />
+            <Explorar />
+          </>
+        }/>
+
+        {/* Mis cursos */}
+        <Route path="/mis-cursos" element={
+          <>
+            <Navbar currentSection="courses" />
+            <MisCursos />
+          </>
+        }/>
+
+        {/* Info de curso */}
+        <Route path="/curso/:id" element={
+          <>
+            <Navbar currentSection="courses" />
+            <InfoCurso />
+          </>
+        }/>
+
+        {/* PAYMENTS SERVICE PANEL */}
+        <Route path="/payments-service-panel" element={<PaymentsDashboard />} />
+
+        {/* Default */}
+        <Route path="*" element={<Navigate to="/payments-service-panel" replace />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
